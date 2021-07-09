6e0a877df2ac9c75da825378ce83980586c0637f
LukeMcCann <developer-lukemccann@outlook.com>
2021-07-09 23:21:24 +0100

    Fix show endpoint

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-let genres = {};
+let genres = [];
 
 fs.readFile(`${__dirname}/data/data.json`, (err, data) => {
     if (err) throw err;
@@ -18,11 +18,16 @@ fs.readFile(`${__dirname}/data/data.json`, (err, data) => {
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    return res.send('Welcome to Vidly API!');
+    return res.status(200).send('Welcome to Vidly API!');
 });
 
 app.get('/api/genres', (req, res) => {
-    return res.send(genres);
+    return res.status(200).send(genres);
+});
+
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    return !genre ? res.status(404).send('404 Not Found!') : res.status(200).send(genre);
 });
 
 app.listen(port, () => {
