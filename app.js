e4db3c37c935eb1af5223d9a82645d0d0f8254aa
LukeMcCann <developer-lukemccann@outlook.com>
2021-07-09 23:32:00 +0100

    Add post and valdiate

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 
 const fs = require('fs');
 const express = require('express');
+const Joi = require('joi');
 const app = express();
 
 let genres = [];
@@ -29,6 +30,25 @@ app.get('/api/genres/:id', (req, res) => {
     const genre = genres.find(g => g.id === parseInt(req.params.id));
     return !genre ? res.status(404).send('404 Not Found!') : res.status(200).send(genre);
 });
+
+app.post('/api/genres', (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+    });
+
+    const result = schema.validate(req.body);
+
+
+    const genre = {
+        id: genres.length+1, 
+        name: req.body.name,
+    };
+
+    genres.push(genre);
+    return res.status(201).send(genre);
+});
+
+
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
